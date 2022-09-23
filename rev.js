00ac83d8acd4a593510f47edcfe69ca1026c9fba
elavarasa03 <elavarasan03srivi@gmail.com>
2022-09-23 11:09:01 +0530

    first commit

---
+++ rev.js
@@ -0,0 +1,20 @@
+// let a=prompt("Enter any Value");
+// let b,sum=0;
+// while(a>0)
+// {
+//     b=a%10;
+//     sum=sum*10+b;
+//     a=parseInt(a/10)
+// }
+// alert(sum);
+
+
+// let date_1 = new Date('03/22/2029');
+// let date_2 = new Date();
+// console.log(date_1);
+// const days = (date_1, date_2) =>{
+//     let difference = date_1.getTime() - date_2.getTime();
+//     let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+//     return TotalDays;
+// }
+// console.log(days(date_1, date_2) +" days to world cup");
